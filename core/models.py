bf9e0e86d33f79f85c468e5f57d9a0a16c4e1eba
SangMin <lsmsk014@gmail.com>
2022-08-04 19:36:29 +0900

    study 220731-220804

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class TimeStampedModel(models.Model):
+    
+    """ Time Stamped Model """
+
+    created = models.DateTimeField(auto_now_add=True) # auto_now_add -> 모델이 생성된 날짜
+    updated = models.DateTimeField(auto_now=True) # auto_now -> 모델을 업데이트 할 때마다 바꿔줌 
+
+    class Meta:
+        abstract = True
